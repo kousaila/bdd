b9da7bcaed10369f269ace018c7f6f005c829ab6
kousaila <kousailatidjet@gmail.com>
2019-05-01 15:29:52 +0200

    deuxieme version

--- eclipse_java/tp5bdd/src/tp5BDD/Mon_interface.java
+++ eclipse_java/tp5bdd/src/tp5BDD/Mon_interface.java
@@ -11,67 +11,146 @@ import java.sql.Statement;
 import javax.swing.*;
 
 public class Mon_interface extends JFrame implements ActionListener {
-
-private TextField mStat, m1, m2, m3, m4,id,mdp,nom_parcours,idcours,nom_cours,an_etude;
-    
-    TextArea mRes;
-    private Button b1, b2, b3, b4,b5,insert_livre,insert_parcours,insert_cours, cours_parcours,livre_cours;    
-	private static final long serialVersionUID = 1L; 
+/*
+	 * 
+	 */
+	// TODO les attributs utilisés
 	private boolean a_verification=false;
-	static Connection connexion;
-	static Statement stmt ;
-
-	// TODO Auto-generated constructor stub
-	private JTabbedPane tp;
-	private JPanel pou,pod,pot,pn,ps,pc;
-	private JScrollPane sp;
+			private JTabbedPane tp;
+			private JPanel pou,insertion,recherche,pn,ps,pc;
+			private JScrollPane sp;
+			private JCheckBox c_cours,c_parcours;
+	        private TextField mStat, ref_livre, auteur_livre, t_id_parcours, m_query,id,mdp,nom_parcours,idcours,nom_cours,an_etude,idens,mdp_ens,nom_ens;
+		    private TextField r_nom_cours;
+		    private JComboBox liste_parcours,r_liste_parcours;
+		
+		    TextArea mRes;
+		    private Button b1, b2, b3, b4,b5,insert_livre,insert_ens,insert_parcours,insert_cours, cours_parcours,livre_cours,chercher;
+	        private static final long serialVersionUID = 1L; 
+		
+		    static Connection connexion;
+		    static Statement stmt ;
 	public Mon_interface() {
-		setTitle("Fenetre avec onglet");
-		setSize(650, 750);
+		setTitle("bibliothque");
+		setSize(720, 700);
 		setResizable(false);
 		setLocationRelativeTo(this);
-
+		
 		tp=new JTabbedPane();
 		//sp =new JScrollPane(tp,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		add(tp);
-
-
-
-		//onglet222222222222222222222222222222222222222222222222222
-		pod=new JPanel();
-
-
-		tp.add(pod,"insertion");
-		// insertion des cours
-        idcours=new TextField(30);
-        idcours.setText("identifiant du cours");
-        nom_cours=new TextField(45);
-        nom_cours.setText("nom du cours");
-        an_etude=new TextField("année d'étude ");
-        insert_cours=new Button("insert cours");
-        insert_cours.addActionListener(this);
-		//onglet333333333333333333333333333333333333333333333333333333333333333333333333333333
-		pot=new JPanel();
-		tp.add(pot,"recherche");
-		pot.setBackground(Color.LIGHT_GRAY);
-
+		//onglet recheche
+				recherche=new JPanel();
+				tp.add(recherche,"recherche");
+				recherche.setBackground(Color.LIGHT_GRAY);
+				r_liste_parcours=new JComboBox<>();
+				r_liste_parcours=new JComboBox<>();
+				r_liste_parcours.addItem("Info");
+				r_liste_parcours.addItem("Materiaux");
+				r_liste_parcours.addItem("IoT");
+				r_liste_parcours.addItem("télécom et réseau");
+				r_liste_parcours.addItem("IS");
+				chercher=new Button("chercher");
+				chercher.addActionListener(this);
+				r_nom_cours= new TextField(25);
+				r_nom_cours.setText("nom du cours");
+				c_cours=new JCheckBox();
+				c_parcours=new JCheckBox();
+				c_cours.addActionListener(this);
+				c_parcours.addActionListener(this);
+				
+				
+		
+	
+	
+	//ongletd'insertion
+		insertion=new JPanel();
+		tp.add(insertion,"insertion");
+		insertion.setLayout(new BorderLayout());
+		pn =new JPanel();
+		ps= new JPanel();
+		insertion.add(pn,BorderLayout.NORTH);
+		pc=new JPanel();
+		insertion.add(pc,BorderLayout.CENTER);
+		insertion.add(ps,BorderLayout.SOUTH);
+		
+		
+		
+		
+	
+		
 		// mon code
 		mStat = new TextField(80);
 		mStat.setEditable(false);
-
-
-		m1 = new TextField(80);
-		m2 = new TextField(80);
-		m3 = new TextField(80);
-		m4 = new TextField(80);
+		
+		//authentification et connexion
+		
+	
+		// insertion des cours
+		idcours=new TextField(30);
+		idcours.setText("identifiant du cours");
+		nom_cours=new TextField(45);
+		nom_cours.setText("nom du cours");
+		an_etude=new TextField("année d'étude ");
+		insert_cours=new Button("insert cours");
+		insert_cours.addActionListener(this);
+		
+	
+		//insertion enseignant
+		idens=new TextField(30);
+		idens.setText("identifiant de l'enseignant");
+		mdp_ens=new TextField(45);
+		mdp_ens.setText("mot de passe de l'enseignant");
+		nom_ens=new TextField(45);
+		nom_ens.setText("nom de l'enseignant");
+		insert_ens=new Button("insert enseignat");
+		insert_ens.addActionListener(this);
+		
+		
+	// insertion des livre
+		insert_livre= new Button("inserer livre");
+		insert_livre.addActionListener(this);
+		ref_livre = new TextField(80);
+		auteur_livre = new TextField(80);
+		ref_livre.setText("reference du livre");  //According to the database schema
+		auteur_livre.setText("Auteur du livre"); //According to the database schema
+		
+		//insertion des parcours
+		liste_parcours=new JComboBox<>();
+		liste_parcours.addItem("Info");
+		liste_parcours.addItem("Materiaux");
+		liste_parcours.addItem("IoT");
+		liste_parcours.addItem("télécom et réseau");
+		liste_parcours.addItem("IS");
+		/*t_id_parcours = new TextField(80);
+		nom_parcours=new TextField(80);
+		insert_parcours= new Button("inserer parcours");
+		insert_parcours.addActionListener(this);
+		t_id_parcours.setText(" identifiant du parcours");  //According to the database schema
+		nom_parcours.setText("le nom du parcours");*/
+		//authentification
+		
 		id = new TextField(20);
 		mdp= new TextField(30);
-		mRes = new TextArea(10,80);
-
-		nom_parcours=new TextField(80);
+		mRes = new TextArea(30,80);
+		id.setText("identifiant");
+		mdp.setText("mot de passe");
+		b5= new Button("s'authentifier");
+		b5.addActionListener(this);
+		
+		
+		//query
+		m_query = new TextField(80);
 		mRes.setEditable(false);
+		m_query.setText("Insert Query here");
+		mRes.setText("Query results");
+		// les association
+		cours_parcours= new Button("associer cours et parcours");
+		livre_cours= new Button("associer livre et cours");
+		cours_parcours.addActionListener(this);
+		livre_cours.addActionListener(this);
 
-
+		
 
 		/**
 		 * First we define the buttons, then we add to the Applet, finally add and ActionListener 
@@ -80,61 +159,71 @@ private TextField mStat, m1, m2, m3, m4,id,mdp,nom_parcours,idcours,nom_cours,an
 		b1 = new Button("CONNECT");
 		b2 = new Button("DISCONNECT");
 		b3 = new Button("QUERY");
-		b5= new Button("s'authentifier");
-		insert_cours= new Button("inserer cours");
-		insert_parcours= new Button("inserer parcours");
-		insert_livre= new Button("inserer livre");
-		cours_parcours= new Button("associer cours et parcours");
-		livre_cours= new Button("associer livre et cours");
-		pod.add(id);
-		pod.add(mdp);
-		pod.add(b5);
-		pod.add(b1) ;
-		pod.add(b2) ;
-		pod.add(b3) ;
-
+		
+		
+		
+		
+		
+		
+		pn.add(id);
+		pn.add(mdp);
+		pn.add(b5);
+		pn.add(b1) ;
+		pn.add(b2) ;
+		recherche.add(b3) ;
+		
 		b1.addActionListener(this);
 		b2.addActionListener(this);
 		b3.addActionListener(this);
-		b5.addActionListener(this);
-		insert_cours.addActionListener(this);
-		insert_livre.addActionListener(this);
-		insert_parcours.addActionListener(this);
-		cours_parcours.addActionListener(this);
-		livre_cours.addActionListener(this);
-		pod.add(mStat);
-		pod.add(new Label("                                                Input cours                                                       "));
-        pod.add(idcours);
-        pod.add(nom_cours);
-        pod.add(an_etude);
-        pod.add(insert_cours);
-		pod.add(new Label("                                                         Input livre:                                                                        ", Label.CENTER));
-		pod.add(m1);
-		pod.add(m2);
-		pod.add(insert_livre);
-		pod.add(new Label("	                                                               Input parcours                                                                                ", Label.CENTER));
-		pod.add(m3);
-		pod.add(nom_parcours);
-		pod.add(insert_parcours);
-		pod.add(new Label("                                                         associer un livre à un cours:                                     ", Label.CENTER));
-
-		pod.add(livre_cours);
-		pod.add(new Label("associer un cours à un à un parcours: ", Label.CENTER));
-		pod.add(cours_parcours);
+		
+		
+		
+	
+		
+		//insertion cours
+		pc.add(new Label("                                               iinput cours                                                       ",Label.CENTER));
+		pc.add(idcours);
+		pc.add(nom_cours);
+		pc.add(an_etude);
+		pc.add(insert_cours);
+		//insertion enseignat
+		pc.add(new Label("                                              input enseignant                                                       "));
+		pc.add(idens);
+		pc.add(mdp_ens);
+		pc.add(nom_ens);
+		pc.add(insert_ens);
+		//insertion status
+		pc.add(mStat);
+		//insertion livre
+		pc.add(new Label("                                                         Input livre:                                                                        ", Label.CENTER));
+		pc.add(ref_livre);
+		pc.add(auteur_livre);
+		pc.add(insert_livre);
+		//insertion parcours
+		pc.add(new Label("  Input parcours   ", Label.CENTER));
+		pc.add(liste_parcours);
+		//insertion.add(t_id_parcours);
+		//insertion.add(nom_parcours);
+		//insertion.add(insert_parcours);
+		//association
+		ps.add(new Label(" associations:  ", Label.CENTER));
+		
+		ps.add(livre_cours);
+		ps.add(cours_parcours);
 		// la reQUETTE
-		pod.add(m4);
-		m1.setText("reference du livre");  //According to the database schema
-		m2.setText("Auteur du livre"); //According to the database schema
-		m3.setText(" identifiant du parcours");  //According to the database schema
-		nom_parcours.setText("le nom du parcours");
-		m4.setText("Insert Query here");
-		id.setText("identifiant");
-		mdp.setText("mot de passe");
-		pod.add(new Label("Query results: ", Label.CENTER));
-		pod.add(mRes);
-		mRes.setText("Query results");
+		recherche.add(m_query);
+		recherche.add(c_cours);
+		recherche.add(r_nom_cours);
+		
+		recherche.add(c_parcours);
+		recherche.add(r_liste_parcours);
+		
+		recherche.add(chercher);
+		recherche.add(mRes);
+		
+		
 
-
+	
 	}
 	public void actionPerformed(ActionEvent event)
 	{
@@ -240,7 +329,7 @@ private TextField mStat, m1, m2, m3, m4,id,mdp,nom_parcours,idcours,nom_cours,an
 		try {
 			Statement m_stmt = connexion.createStatement();
 			String sql;
-			sql = m4.getText();
+			sql = m_query.getText();
 			ResultSet rs = stmt.executeQuery(sql);
 			setStatus("Querying the database");
 			mRes.setText("The query result is presented here.\n");
@@ -261,8 +350,8 @@ private TextField mStat, m1, m2, m3, m4,id,mdp,nom_parcours,idcours,nom_cours,an
 	private void insertDatabase(){
 		try{
 			stmt = connexion.createStatement();
-			String name = m1.getText();
-			String age = m2.getText();
+			String name = ref_livre.getText();
+			String age = auteur_livre.getText();
 			String color = m3.getText();
 			String insert = "Insert  INTO  values('" + name + "' , '" + age 
 					+"' , '" + color + "');";
@@ -279,8 +368,8 @@ private TextField mStat, m1, m2, m3, m4,id,mdp,nom_parcours,idcours,nom_cours,an
 	private void insertLivre(){
 		try{
 			stmt = connexion.createStatement();
-			String ref = m1.getText();
-			String author = m2.getText();
+			String ref = ref_livre.getText();
+			String author = auteur_livre.getText();
 			String insert = "Insert  INTO `base_17002447`.`livre` (`ref`, `auteur`) values (\'" + ref + "\', \'" + author 
 					+"\');";
 			stmt.executeUpdate(insert);
@@ -295,7 +384,7 @@ private TextField mStat, m1, m2, m3, m4,id,mdp,nom_parcours,idcours,nom_cours,an
 	private void insertParcours(){
 		try{
 			stmt = connexion.createStatement();
-			String identifiant = m3.getText();
+			String identifiant = t_id_parcours.getText();
 			String nom = nom_parcours.getText();
 			String insert = "Insert  INTO `base_17002447`.`Parcours` values('" + identifiant + "' , '" + nom 
 					+"');";
@@ -312,7 +401,7 @@ private TextField mStat, m1, m2, m3, m4,id,mdp,nom_parcours,idcours,nom_cours,an
 		try{
 			stmt = connexion.createStatement();
 			String idCours = idcours.getText();
-			String idParcours = m3.getText();
+			String idParcours = t_id_parcours.getText();
 			String insert = "Insert  INTO `base_17002447`.`cours_has_parcours` values('" + idCours + "' , '" + idParcours 
 					+"');";
 			stmt.executeUpdate(insert);
@@ -328,7 +417,7 @@ private TextField mStat, m1, m2, m3, m4,id,mdp,nom_parcours,idcours,nom_cours,an
 	private void insertLienLC(){
 		try{
 			stmt = connexion.createStatement();
-			String idLivre = m1.getText();
+			String idLivre = ref_livre.getText();
 			String idCours = idcours.getText();
 			String insert = "Insert  INTO `base_17002447`.`cours_has_livre` (`idcours`, `ref`) values('" + idCours + "' , '" + idLivre 
 					+"');";
@@ -373,9 +462,9 @@ private TextField mStat, m1, m2, m3, m4,id,mdp,nom_parcours,idcours,nom_cours,an
 		/**
 		 * Definition of text fields
 		 */
-		//m1 = new TextField(80);
-		//m1.setText("What are you going to do when the light is:");
-		//m1.setEditable(false);
+		//ref_livre = new TextField(80);
+		//ref_livre.setText("What are you going to do when the light is:");
+		//ref_livre.setEditable(false);
 		Mon_interface ong=new Mon_interface();
 		ong.setVisible(true);
 	}
